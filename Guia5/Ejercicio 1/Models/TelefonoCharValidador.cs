6efb5e42da2f82af2278810a4d337b22a1004cad
alevittNote <alejandrovitt@hotmail.com>
2025-09-17 23:28:25 -0300

    agregue carpeta para las clases, modele clase Validardor, comence con la clase validadorchartelefono y falta escribir su VerMensaje

---
+++ Guia5/Ejercicio 1/Models/TelefonoCharValidador.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1.Models
+{
+    internal class TelefonoCharValidador : Validador
+    {
+
+
+        public TelefonoCharValidador(string expresion) : base(expresion)
+        {
+        }
+
+        public override bool Validar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string VerMensaje()
+        {
+            return base.VerMensaje();
+        }
+        
+    }
+}
---
+++ Guia5/Ejercicio 1/Models/Validador.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1.Models
+{
+     abstract internal class Validador
+    {
+        string expresion;
+
+        public Validador(string expresion) 
+        {
+            this.expresion = expresion;
+        }
+
+        abstract public bool Validar();
+
+        virtual public string VerMensaje()
+        {
+            return expresion;
+        }
+    }
+}
