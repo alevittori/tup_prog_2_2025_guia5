f58745b536a3e7b6f16b15311e7341a3520a5a5c
alevittNote <alejandrovitt@hotmail.com>
2025-09-26 10:43:07 -0300

    finalizamos el ejercicio 4 de la guia

---
+++ Guia5/Ejercicio4/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Ejercicio4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(5, 82);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(772, 404);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(596, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(657, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 52);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Buscar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(280, 502);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(222, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Cantidad de Palabras encontradas: ";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Location = new System.Drawing.Point(508, 489);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 42);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(796, 540);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Guia5/Ejercicio4/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label2.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string frase = "La ratona a partir de las definiciones clasifica las piezas, a partir de las definiciones la ratona toma las piezas \r\ny construye nuevos objetos, y la ratona con la definiciones controla que la interacción \r\nentre los objetos sean las de esperar por sus propias definiciones \r\n";
+            string palabra = textBox1.Text;
+            string patron = $@"\b(?<palabra>{Regex.Escape(palabra)})\b";
+            string reemplazo = @"{\b ${palabra}}";
+            string resultado = Regex.Replace(frase, patron, reemplazo, RegexOptions.IgnoreCase);
+            richTextBox1.Rtf = $@"{{\rtf1\ansi {resultado}}}";
+            int cantidad = Regex.Matches(frase, patron, RegexOptions.IgnoreCase).Count;
+            label2.Text = cantidad.ToString();
+            label2.Visible = true;
+
+        }
+    }
+}
